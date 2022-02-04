0d016ea2d5ec0b36f71955301a1f22ddb31991af
medvedevvyavit <medvedevvyavit@outlook.com>
2022-02-04 15:08:52 +0300

    Created app

---
+++ src/test/java/ru/development/pharmodem/PharmodemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.development.pharmodem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmodemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
